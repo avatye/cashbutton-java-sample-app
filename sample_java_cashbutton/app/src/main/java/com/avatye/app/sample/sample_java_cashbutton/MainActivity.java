02f8247555a56cf86fec94eb1d5950b1a21506c0
yspark <yspark@avatye.com>
2021-04-20 11:59:03 +0900

    Sample App #JAVA  - 최신화(v1.3.14)

--- sample_java_cashbutton/app/src/main/java/com/avatye/app/sample/sample_java_cashbutton/MainActivity.java
+++ sample_java_cashbutton/app/src/main/java/com/avatye/app/sample/sample_java_cashbutton/MainActivity.java
@@ -1,38 +1,41 @@
 package com.avatye.app.sample.sample_java_cashbutton;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
 import android.widget.CompoundButton;
 import android.widget.LinearLayout;
 import android.widget.Switch;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.avatye.sdk.cashbutton.CashButtonConfig;
 import com.avatye.sdk.cashbutton.ICashButtonBackPressedListener;
 import com.avatye.sdk.cashbutton.ICashButtonCallback;
 import com.avatye.sdk.cashbutton.ui.CashButtonLayout;
 
-import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity implements CompoundButton.OnCheckedChangeListener {
 
     private HeaderView component_header_view;
 
     private LinearLayout ly_inquire_container;
-    private CashButtonLayout cashButton;
-    private Boolean isCashButtonState = CashButtonConfig.getCashButtonState();
+    private Switch sw_cashbutton_state, sw_notibar_state;
 
+    private CashButtonLayout cashButtonLayout;
 
     @Override
     public void onBackPressed() {
-        if (cashButton != null) {
-            cashButton.onBackPressed(b -> {
-                if (b) {
-                    finish();
+        if (cashButtonLayout != null) {
+            cashButtonLayout.onBackPressed(new ICashButtonBackPressedListener() {
+                @Override
+                public void onBackPressed(boolean b) {
+                    if (b) {
+                        finish();
+                    }
                 }
             });
+        } else {
+            finish();
         }
     }
 
@@ -42,8 +45,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // region { view inflate }
+        // region { findViewById }
         component_header_view = findViewById(R.id.component_header_view);
+        sw_cashbutton_state = findViewById(R.id.sw_cashbutton_state);
+        sw_notibar_state = findViewById(R.id.sw_notibar_state);
         ly_inquire_container = findViewById(R.id.ly_inquire_container);
         // endregion
 
@@ -51,33 +56,49 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         initCommon();
         // endregion
 
-        // region {캐시버튼 문의}
-        ly_inquire_container.setOnClickListener(this);
-        // endregion}
+        // region { 캐시버튼 설정 }
+        sw_cashbutton_state.setOnCheckedChangeListener(this);
+        // endregion
+
+        // region { 노티바 설정 }
+        sw_notibar_state.setOnCheckedChangeListener(this);
+        // endregion
+
+        // region { 캐시버튼 문의 }
+        ly_inquire_container.setOnClickListener(view -> CashButtonConfig.actionSuggestion(this));
+        // endregion
+
     }
 
 
     private void initCommon() {
-        component_header_view.setBack(view -> onBackPressed());
         CashButtonLayout.init(MainActivity.this, new ICashButtonCallback() {
             @Override
-            public void onSuccess(@NotNull CashButtonLayout cashButtonLayout) {
-                cashButton = cashButtonLayout;
+            public void onSuccess(@Nullable CashButtonLayout cashButtonLayout) {
+                MainActivity.this.cashButtonLayout = cashButtonLayout;
             }
         });
 
-        // region {친구초대 메시지 (커스타미이징)}
-        CashButtonConfig.initInviteInfo("캐시 버튼에서 친구 초대를 사용할 때 사용하는 메시지입니다.");
-        // endregion
+        component_header_view.setBack(view -> onBackPressed());
+
+        CashButtonConfig.initInviteInfo("캐시 버튼에서 친구 초대를 할 때 사용하는 메시지입니다.");
+
+        sw_cashbutton_state.setChecked(CashButtonConfig.getCashButtonState());
+        sw_notibar_state.setChecked(CashButtonConfig.getAllowNotificationBar());
     }
 
 
     @Override
-    public void onClick(View v) {
-        if (v.getId() == R.id.ly_inquire_container) {
-            // region { 캐시버튼 문의 }
-            CashButtonConfig.actionSuggestion(this);
-            // endregion
+    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+        if (buttonView.getId() == R.id.sw_cashbutton_state) {
+            if (isChecked) {
+                CashButtonConfig.setCashButtonSnoozeOff();
+            } else {
+                CashButtonConfig.setCashButtonSnoozeOn(1);
+            }
+        } else if (buttonView.getId() == R.id.sw_notibar_state) {
+            CashButtonConfig.setAllowNotificationBar(this, isChecked);
         }
     }
+
 }
